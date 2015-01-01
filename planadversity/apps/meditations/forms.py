77b51606257b21d2442a10929c8959b7bd9a6aa1
Colin Powell <colin.powell@gmail.com>
2015-01-01 14:24:54 -0500

    Fixes not being able to add meditations on the homepage

--- planadversity/apps/meditations/forms.py
+++ planadversity/apps/meditations/forms.py
@@ -5,16 +5,9 @@ from .models import Response, Meditation
 
 
 class ResponseForm(forms.ModelForm):
-    meditation = forms.IntegerField(required=False, widget=forms.HiddenInput())
+    #meditation = forms.IntegerField(required=False, widget=forms.HiddenInput())
 
     class Meta:
         model = Response
         exclude = ['user']
-
-    def clean(self):
-        cleaned_data = super(ResponseForm, self).clean()
-        try:
-            cleaned_data['meditation'] = Meditation.objects.get(id=cleaned_data['meditation'])
-        except:
-            pass
 
